bcd3aa3f376ba21f7b3feff24d94c47ba66d237d
Hrach <hr.ovakimyan.03@gmail.com>
2020-04-13 22:05:58 +0400

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes } from './routes'
+
+import GlobalContext from './context/Global/Global.context'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalContext.Provider value={{}}>
+      <Router>
+        { Routes() }
+      </Router>
+    </GlobalContext.Provider>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Switch, Redirect  } from 'react-router-dom'
+
+import { Index } from './pages/Index'
+export const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/">
+        <Index /> 
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  )
+}
